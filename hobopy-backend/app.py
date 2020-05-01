1de1ca59f23ebf5914b0f5588c47da6c66dd33be
takeuchikatsuchi <k64katu195@gmail.com>
2020-05-01 23:16:01 +0900

    first-commit

---
+++ hobopy-backend/app.py
@@ -0,0 +1,64 @@
+from chalice import BadRequestError, Chalice, NotFoundError
+from chalicelib import database
+
+app = Chalice(app_name='hobopy-backend')
+
+# 全てのTodoを取得する
+@app.route('/todos', methods=['GET'])
+def get_all_todos():
+    return database.get_all_todos()
+
+# todoを登録する
+@app.route('/todos', methods=['POST'])
+def create_todo():
+    # リクエストメッセージボディを取得する
+    todo = app.current_request.json_body
+
+    # 必須項目をチェックする
+    for key in ['title', 'memo', 'priority']:
+        if key not in todo:
+            raise BadRequestError(f"{key} is required.")
+
+    # データを登録する
+    return database.create_todo(todo)
+
+# 指定されたIDのレコードを取得する
+@app.route('/todos/{todo_id}', methods=['GET'])
+def get_todo(todo_id):
+    todo = database.get_todo(todo_id)
+    if todo:
+        return todo
+    else:
+        raise NotFoundError('Todo not found.')
+
+@app.route('/todos/{todo_id}', methods =['PUT'])
+def update_todo(todo_id):
+    changes = app.current_request.json_body
+
+    # データを更新する
+    return database.update_todo(todo_id, changes)
+
+
+
+
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.current_request.json_body
+#     # We'll echo the json body back to the user in a 'user' key.
+#     return {'user': user_as_json}
+#
+# See the README documentation for more examples.
+#
---
+++ hobopy-backend/chalicelib/database.py
@@ -0,0 +1,65 @@
+import os
+import boto3
+import uuid
+from boto3.dynamodb.conditions import Key
+
+# DynamoDBへの接続を取得する
+def _get_database():
+    endpoint = os.environ.get('DB_ENDPOINT')
+    if endpoint:
+        return boto3.resource('dynamodb', endpoint_url=endpoint)
+    else:
+        return boto3.resource('dynamodb')
+
+# 全てのレコードを取得する
+def get_all_todos():
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    response = table.scan()
+    return response['Items']
+
+def create_todo(todo):
+    # 登録内容を作成する
+    item = {
+        'id': uuid.uuid4().hex,
+        'title': todo['title'],
+        'memo': todo['memo'],
+        'priority': todo['priority'],
+        'completed': False,
+    }
+
+    # DynamoDBにデータを登録する
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    table.put_item(Item=item)
+    return item
+
+# 指定されたIDのレコードを取得する
+def get_todo(todo_id):
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+    response = table.query(
+        KeyConditionExpression= Key('id').eq(todo_id))
+    items = response['Items']
+    return items[0] if items else None
+
+#  指定されたIDのToDoを更新する
+def update_todo(todo_id, changes):
+    table = _get_database().Table(os.environ['DB_TABLE_NAME'])
+
+    # クエリを構築する
+    update_expression = []
+    expression_attribute_values = {}
+    for key in ['title', 'memo', 'priority', 'completed']:
+        if key in changes:
+            update_expression.append(f"{key} = :{key[0:1]}")
+            expression_attribute_values[f": {key[0:1]}"] = changes[key]
+
+            # 2.DynamoDBのデータを更新する
+            result = table.update_item(
+                Key={
+                    'id': todo_id,
+                },
+                UpdateExpression = 'set  ' + ','.join(update_expression),
+                ExpressionAttributeValues=expression_attribute_values,
+                ReturnValues ='ALL_NEW'
+            )
+            return result['Attributes']
+
